9eb45a5a8ab470cb44f78288a59d0e84fe550658
Ramil Mavlikhanov <ramil040201@gmail.com>
2025-02-24 18:29:24 +0300

    ADD: exoplayer

--- app/src/main/java/com/example/edu/videotesttask/presentation/player/PlayerFragment.kt
+++ app/src/main/java/com/example/edu/videotesttask/presentation/player/PlayerFragment.kt
@@ -1,12 +1,15 @@
 package com.example.edu.videotesttask.presentation.player
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.navigation.fragment.navArgs
-import com.example.edu.videotesttask.R
 import com.example.edu.videotesttask.databinding.FragmentPlayerBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,7 +30,18 @@ class PlayerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.test.text = args.URL
+
+        val player = ExoPlayer.Builder(requireContext()).build()
+        binding.playerView.player = player
+
+        val uri = "${args.URL}/download".toUri()
+
+        Log.d("URI_VIDEO", "$uri")
+
+        val mediaItem = MediaItem.fromUri(uri)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
     }
 
     override fun onDestroy() {
